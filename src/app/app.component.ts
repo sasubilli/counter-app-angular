9a980b411b952508daeab5dcc738894e2e13f481
Bannu <55312133+sasubilli@users.noreply.github.com>
2020-08-02 16:25:45 +0530

    basics-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = "Counter App";
+
+count: number = 0;
+
+handleIncrease = () =>{
+
+  if (this.count === 10) {
+    this.count=this.count +15;
+  }
+  this.count = this.count +1;
+};
+
+handleDecrease = () =>{
+  this.count = this.count -1;
+};
+
+handleReset = () => {
+  this.count = 0;
+};
+
 }
